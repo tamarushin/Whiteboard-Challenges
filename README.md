d54d298306f59c036ebf3c516988d04f9b44d35f
tamarushin <tamarushin@gmail.com>
2018-02-22 14:44:47 -0800

    wb-09-solution

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Requirements
-Given an array of integers, which contains numbers from 1 to 100 and one number is missing, write a function that will find the missing number and return it.
+Create a function that accepts 2 presorted arrays of integers as it's input. Your function should merge these 2 arrays and return a final single sorted array.
 
-ex: findMissing = (arr) => {...}
+Note: The length of either array does not matter. All integers in the array will be whole numbers with a positive value.
+
+For example:
+
+// given:
+[2, 5, 13, 21]
+
+// and:
+[3, 19, 27, 31]
+
+// return:
+[2, 3, 5, 13, 19, 21, 27, 31]
 
--- solution.js
+++ solution.js
@@ -1,19 +1,42 @@
 'use strict';
 
-let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15];
-let missNum;
+function sortTwoArrays(arr1, arr2) {
+  let j = 0; //
+  let solution = [];
 
-let find = function (arr1) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (i !== arr1[i + 1]) {
-      
-      missNum = i + 1;
-
+  for (i = 0; i < arr1.length;) {
+    if (arr2[j] < arr1[i]) {
+      solution.push(arr2[j]);
+      j++;
+      if (j === arr2.length) {
+        break;
+      }
+    } else if (arr2[j] > arr1[i]) {
+      solution.push(arr1[i]);
+      i++;
+    } else {
+      solution.push(arr1[i]);
+      solution.push(arr2[j]);
+      i++;
+      j++;
+      if (j === arr2.length) {
+        break;
+      }
     }
   }
 
-  console.log(missNum);
-  return missNum;
-};
+  if (j <= arr2.length - 1) {
+    for (j = j; j < arr2.length; j++) {
+      solution.push(arr2[j]);
+    }
+  }
 
-find(arr1);
+  if (i <= arr1.length - 1) {
+    for (i = i; i < arr1.length; i++) {
+      solution.push(arr1[i]);
+    }
+  }
+
+
+  return solution;
+}
